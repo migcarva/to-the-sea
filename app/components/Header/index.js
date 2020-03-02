664857fef62d469af15044355a63d78863f11724
Miguel Carvalho <miguel.carvalho@unbabel.com>
2020-03-02 09:54:53 +0000

    Header stuff

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,53 +1,16 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 
-import Icon from 'components/Icon';
-
 import HeaderWrapper from './HeaderWrapper';
-import messages from './messages';
+// import messages from './messages';
 
 function Header() {
-  const links = [
-    {
-      url: '/about',
-      title: 'Our Story',
-    },
-    {
-      url: '/tours',
-      title: 'Tours',
-    },
-    {
-      url: '/testimonials',
-      title: 'Testimonials',
-    },
-    {
-      url: '/contact',
-      title: 'Contact',
-    },
-  ];
   return (
     <HeaderWrapper>
       <Link to="/">
         <h1>tothesea</h1>
       </Link>
-      <nav>
-        <ul>
-          {links.map(link => (
-            <li key={link.url}>
-              <Link to={link.url}>{link.title}</Link>
-            </li>
-          ))}
-        </ul>
-        <div>
-          <span>
-            <Icon name="menu" />
-          </span>
-          <span>
-            <Icon name="close" />
-          </span>
-        </div>
-      </nav>
     </HeaderWrapper>
   );
 }
